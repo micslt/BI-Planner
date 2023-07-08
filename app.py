25c4a7c118d236c8b198e66c260bbeaf1918b513
micslt <michael.schlotter@pm.me>
2023-07-08 16:38:21 +0200

    .

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import csv
-import biplanner
 
 app = Flask(__name__)
 
@@ -8,27 +7,67 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route("/fragen1")
-def fragen1():
-    return render_template("questions1.html")
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route("/reifegrad")
+def reifegrad():
+    return render_template("reifegrad.html")
+
+@app.route("/rahmenbedingungen")
+def rahmenbedingungen():
+    return render_template("rahmenbedingungen.html")
+
+@app.route("/unternehmensziele")
+def unternehmensziele():
+    return render_template("unternehmensziele.html")
+
+@app.route("/informationsbedarf")
+def informationsbedarf():
+    return render_template("informationsbedarf.html")
+
+@app.route("/datenquellen")
+def datenquellen():
+    return render_template("datenquellen.html")
+
+@app.route("/datenmanagementkonzept")
+def datenmanagementkonzept():
+    return render_template("datenmanagementkonzept.html")
+
+@app.route("/etl")
+def etl():
+    return render_template("etl.html")
+
+@app.route("/analysen")
+def analysen():
+    return render_template("analysen.html")
+
+@app.route("/visualisierung")
+def visualisierung():
+    return render_template("visualisierung.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/auswertungen")
+def auswertungen():
+    return render_template("auswertungen.html")
+
 
 @app.route("/submit", methods=["POST"])
 def submit_form():
     # Die Antworten aus dem Formular abrufen
-    analytics_answer1 = request.form.get("analytics_answer1")
-    analytics_answer2 = request.form.get("analytics_answer2")
-    governance_answer1 = request.form.get("governance_answer1")
-    governance_answer2 = request.form.get("governance_answer2")
-    # Weitere Antworten für andere Fragen abrufen
+    frage1 = request.form.get("frage1")
+    frage2 = request.form.get("frage2")
+    frage3 = request.form.get("frage3")
+    frage4 = request.form.get("frage4")
+    frage5 = request.form.get("frage5")
+    frage6 = request.form.get("frage6")
 
     # Eine Liste mit den Antworten erstellen
-    answers = [
-        analytics_answer1,
-        analytics_answer2,
-        governance_answer1,
-        governance_answer2,
-        # Weitere Antworten für andere Fragen hinzufügen
-    ]
+    answers = [frage1, frage2, frage3, frage4, frage5, frage6]
 
     # CSV-Datei öffnen und Antworten schreiben
     filename = "antworten.csv"
@@ -36,8 +75,9 @@ def submit_form():
         writer = csv.writer(csvfile)
         writer.writerow(answers)
 
-    # Eine Bestätigungsseite oder eine Weiterleitung anzeigen
-    return "Vielen Dank für Ihre Antworten!"
+    # Nachricht anzeigen und zur Startseite (index) umleiten
+    message = "Ihre Antworten wurden erfasst. Sie werden weitergeleitet."
+    return render_template("message.html", message=message, redirect_url="/index")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
