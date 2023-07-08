d87c8a7eb9ef69dd4e055a05577cb5f1fe9ee985
micslt <michael.schlotter@pm.me>
2023-07-08 18:01:16 +0200

    .

--- app.py
+++ app.py
@@ -80,4 +80,6 @@ def submit_form():
     return render_template("message.html", message=message, redirect_url="/index")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run()
+#if __name__ == '__main__':
+#    app.run(debug=True, port=5000)"
