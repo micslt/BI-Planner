ab3527286d67db0210e4dbb13144440dec38062d
micslt <michael.schlotter@pm.me>
2023-07-09 11:45:49 +0200

    .

--- app.py
+++ app.py
@@ -11,14 +11,13 @@ def home():
 def index():
     return render_template('index.html')
 
-@app.route("/reifegrad")
-def reifegrad():
-    return render_template("reifegrad.html")
 
 @app.route("/rahmenbedingungen")
 def rahmenbedingungen():
     return render_template("rahmenbedingungen.html")
 
+
+#############################################################################
 @app.route('/unternehmensziele', methods=['GET', 'POST'])
 def unternehmensziele():
     if request.method == 'POST':
@@ -56,10 +55,58 @@ def delete_ziel():
             writer.writerow([ziel])
 
     return redirect('/unternehmensziele')
+#######################################################################################
 
-@app.route("/informationsbedarf")
+@app.route('/informationsbedarf', methods=['GET', 'POST'])
 def informationsbedarf():
-    return render_template("informationsbedarf.html")
+    if request.method == 'POST':
+        informationsbedarf = request.form['informationsbedarf']
+        beschreibung = request.form['beschreibung']
+        nutzen = request.form['nutzen']
+        typ = request.form['typ']
+        metrik = request.form['metrik']
+        prioritaet = request.form.get('prioritaet', '-')  # Standardwert "-" verwenden, falls nicht angegeben
+
+        with open('informationsbedarf.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([informationsbedarf, beschreibung, nutzen, typ, metrik, prioritaet])
+
+        return redirect('/informationsbedarf')
+    else:
+        try:
+            with open('informationsbedarf.csv', 'r') as csvfile:
+                reader = csv.reader(csvfile)
+                # enumerate() wird verwendet, um sowohl die Informationsbedarfe als auch deren Nummern zu bekommen.
+                informationsbedarf_liste = [(i, row) for i, row in enumerate(reader, start=1)]
+        except FileNotFoundError:
+            informationsbedarf_liste = []
+    return render_template('informationsbedarf.html', informationsbedarf_liste=informationsbedarf_liste)
+
+
+
+@app.route('/informationsbedarf/delete', methods=['POST'])
+def delete_informationsbedarf():
+    informationsbedarf_nummer = request.form['informationsbedarf_nummer']
+
+    # Lesen Sie alle Informationsbedarfe
+    with open('informationsbedarf.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        informationsbedarf = [row[0] for row in reader]
+
+    # Löschen Sie den ausgewählten Informationsbedarf
+    del informationsbedarf[int(informationsbedarf_nummer) - 1]
+
+    # Überschreiben Sie die CSV-Datei mit den verbleibenden Informationsbedarfen
+    with open('informationsbedarf.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for informationsbedarf in informationsbedarf:
+            writer.writerow([informationsbedarf])
+
+    return redirect('/informationsbedarf')
+
+
+#######################################################################################
+
 
 @app.route("/datenquellen")
 def datenquellen():
@@ -92,10 +139,16 @@ def auswertungen():
 @app.route('/delete_all', methods=['POST'])
 def delete_all():
     open('ziele.csv', 'w').close()
-    open('antworten.csv', 'w').close()
+    open('reifegrad.csv', 'w').close()
+    open('informationsbedarf.csv', 'w').close()
     return redirect('/')
+##########################################################################################
 
-@app.route("/submit", methods=["POST"])
+@app.route("/reifegrad")
+def reifegrad():
+    return render_template("reifegrad.html")
+
+@app.route("/reifegrad_submit", methods=["POST"])
 def submit_form():
     # Die Antworten aus dem Formular abrufen
     frage1 = request.form.get("frage1")
@@ -104,19 +157,58 @@ def submit_form():
     frage4 = request.form.get("frage4")
     frage5 = request.form.get("frage5")
     frage6 = request.form.get("frage6")
+    frage7 = request.form.get("frage7")
+    frage8 = request.form.get("frage8")
+    frage9 = request.form.get("frage9")
+    frage10 = request.form.get("frage10")
+    frage11 = request.form.get("frage11")
+    frage12 = request.form.get("frage12")
+    frage13 = request.form.get("frage13")
+    frage14 = request.form.get("frage14")
+    frage15 = request.form.get("frage15")
+    frage16 = request.form.get("frage16")
+    frage17 = request.form.get("frage17")
+    frage18 = request.form.get("frage18")
+    frage19 = request.form.get("frage19")
+    frage20 = request.form.get("frage20")
+    frage21 = request.form.get("frage21")
+    frage22 = request.form.get("frage22")
+    frage23 = request.form.get("frage23")
+    frage24 = request.form.get("frage24")
+    frage25 = request.form.get("frage25")
+    frage26 = request.form.get("frage26")
+    frage27 = request.form.get("frage27")
+    frage28 = request.form.get("frage28")
+    frage29 = request.form.get("frage29")
+    frage30 = request.form.get("frage30")
+    frage31 = request.form.get("frage31")
+    frage32 = request.form.get("frage32")
+    frage33 = request.form.get("frage33")
+    frage34 = request.form.get("frage34")
+    frage35 = request.form.get("frage35")
+    frage36 = request.form.get("frage36")
 
     # Eine Liste mit den Antworten erstellen
-    answers = [frage1, frage2, frage3, frage4, frage5, frage6]
+    answers = [frage1, frage2, frage3, frage4, frage5, frage6, frage7, frage8, frage9,
+               frage10, frage11, frage12, frage13, frage14, frage15, frage16, frage17,
+               frage18, frage19, frage20, frage21, frage22, frage23, frage24, frage25,
+               frage26, frage27, frage28, frage29, frage30, frage31, frage32, frage33,
+               frage34, frage35, frage36]
+
 
     # CSV-Datei öffnen und Antworten schreiben
-    filename = "antworten.csv"
+    filename = "reifegrad.csv"
     with open(filename, "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(answers)
 
     # Nachricht anzeigen und zur Startseite (index) umleiten
     message = "Ihre Antworten wurden erfasst. Sie werden weitergeleitet."
-    return render_template("message.html", message=message, redirect_url="/index")
+    return render_template("message.html", message=message, redirect_url="/auswertungen")
+
+
+
+##############################################################################################
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
