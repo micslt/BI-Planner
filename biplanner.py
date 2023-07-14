bea244498ac1348b4813db4b7a672f898b68cd69
micslt <michael.schlotter@pm.me>
2023-07-14 14:56:15 +0200

    .

--- biplanner.py
+++ biplanner.py
@@ -1,9 +1,6 @@
 import csv
 import os
 
-import csv
-import os
-
 def calculate_points_and_save_to_csv(csv_file):
     # Überprüfen, ob die CSV-Datei vorhanden ist
     csv_exists = os.path.isfile(csv_file)
@@ -14,25 +11,36 @@ def calculate_points_and_save_to_csv(csv_file):
     dateninfrastruktur = []
     analytik = []
     governance = []
+    total = []
+
+    comment_organisation = []
+    comment_ressourcen = []
+    comment_dateninfrastruktur = []
+    comment_analytik = []
+    comment_governance = []
+
     with open("reifegrad.csv", "r", newline="", encoding="utf-8") as csvfile:
         reader = csv.reader(csvfile)
         next(reader)  # Header-Zeile überspringen
         for row in reader:
-            organisation.append(sum([1 if answer == "ja" else 0 for answer in row[0:6]]))
-            ressourcen.append(sum([1 if answer == "ja" else 0 for answer in [row[6]] + row[8:12]]))
-            dateninfrastruktur.append(sum([1 if answer == "ja" else 0 for answer in row[13:19]]))
-            analytik.append(sum([1 if answer == "ja" else 0 for answer in row[13:19]]))
-            governance.append(sum([1 if answer == "ja" else 0 for answer in row[13:19]]))
+            organisation.append(sum([1 if answer == "ja" else 0 for answer in row[1:7]]) / 6 * 100)
+            ressourcen.append(sum([1 if answer == "ja" or answer in ["5"] else 0.75 if answer == "4" else 0.5 if answer == "3" else 0.25 if answer == "2" else 0 for answer in row[7:13]]) / 6 * 100)
+            dateninfrastruktur.append(sum([1 if answer == "ja" else 0 for answer in row[13:19]])/6*100)
+            analytik.append(sum([1 if answer == "ja" or answer in ["5"] else 0.75 if answer == "4" else 0.5 if answer == "3" else 0.25 if answer == "2" else 0 for answer in row[19:25]])/6*100)
+            governance.append(sum([1 if answer == "ja" else 0 for answer in row[25:35]])/10*100)
+            total.append(sum([organisation[0], ressourcen[0], dateninfrastruktur[0], analytik[0], governance[0]]) / 5)
+            comment_organisation.append("Aus Sicht der Organisation erreicht " if organisation[0]>80 else "kacke")
 
     # Die Punkte in die CSV-Datei schreiben
     with open(csv_file, "w", newline="", encoding="utf-8") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["Key", "Organisation", "Ressourcen", "Dateninfrastruktur", "Analytik", "Governance" ])
+        writer.writerow(["Key", "Organisation", "Ressourcen", "Dateninfrastruktur", "Analytik", "Governance", "Total", "Kommentar Organisation" ])
         with open("reifegrad.csv", "r", newline="", encoding="utf-8") as csvfile:
             reader = csv.reader(csvfile)
             next(reader)  # Header-Zeile überspringen
-            for row, points1, points2, points3, points4, points5 in zip(reader, organisation, ressourcen, dateninfrastruktur, analytik, governance):
-                writer.writerow([row[0], points1, points2, points3, points4, points5])
+            for row, points1, points2, points3, points4, points5, points6,  comment1 in zip(reader, organisation, ressourcen,
+                                                                        dateninfrastruktur, analytik, governance, total, comment_organisation):
+                writer.writerow([row[0], points1, points2, points3, points4, points5, points6, comment1])
 
 
 def get_latest_entry():
